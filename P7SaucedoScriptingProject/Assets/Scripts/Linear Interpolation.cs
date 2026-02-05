1eca206749cf6f07f8b5e8eb4b35a3fe0c3b6b9b
Gogmazios <732861@student.dusd.net>
2026-02-05 14:44:36 -0800

    LinearInterpolation
    LinearInterpolation

---
+++ P7SaucedoScriptingProject/Assets/Scripts/Linear Interpolation.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinearInterpolation : MonoBehaviour
+{ /*
+    // Start is called before the first frame update
+    void Start()
+    {
+        //In this case we get 364
+        float result = Math.Lerp(600f, 128f, 0.5f);
+
+
+        Vector3 from = new Vector3(1f, 10f, 100f);
+        Vector3 to = new Vector3(10f, 100f, 1000f);
+
+        // Heres result = (8, 80,800) 
+        Vector result = Vector3.Lerp(from, to, 0.80);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Light>().intensity = Mathf.Lerp(GetComponent<Light>().intensity, 10000f, 100f * Time.deltaTime);
+    }
+    
+   */ 
+}
